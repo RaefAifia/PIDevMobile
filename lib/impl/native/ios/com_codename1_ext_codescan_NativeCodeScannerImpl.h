750d5d7f0a658e3d7c85a50f625b6a63314f4a5e
HelaFersi <78934541+HelaFersi@users.noreply.github.com>
2021-05-18 17:08:03 +0100

    Delete lib directory

--- lib/impl/native/ios/com_codename1_ext_codescan_NativeCodeScannerImpl.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface com_codename1_ext_codescan_NativeCodeScannerImpl : NSObject {
-}
-
--(void)scanQRCode;
--(void)scanBarCode;
--(BOOL)isSupported;
-@end
--- lib/impl/native/ios/org_littlemonkey_qrscanner_NativeScannerImpl.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface org_littlemonkey_qrscanner_NativeScannerImpl : NSObject {
-}
-
--(void)scanQRCode;
--(void)scanBarCode;
--(BOOL)isSupported;
-@end
