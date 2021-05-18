0031f790ae1639c47459b5a25b8db1c586ac3369
kairedinebennour <kairedine.bennour@esprit.tn>
2021-05-18 20:06:53 +0100

    khayri
    khayri

---
+++ src/entities/Blog.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author hp
+ */
+public class Blog {
+    private int id;
+    private String titre;
+    private String image;
+    private String Description;
+
+    public Blog() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+    
+}
---
+++ src/services/Blog_Service.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.l10n.ParseException;
+import com.codename1.ui.events.ActionListener;
+import entities.Blog;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import utils.Statics;
+
+/**
+ *
+ * @author hp
+ */
+public class Blog_Service {
+        public ArrayList<Blog> Blogs;
+    public static Blog_Service instance = null;
+    public boolean resultOK;
+    private ConnectionRequest req;
+     public Blog_Service() {
+        req = new ConnectionRequest();
+    }
+
+    public static Blog_Service getInstance() {
+        if (instance == null) {
+            instance = new Blog_Service();
+        }
+        return instance;
+    }
+    
+      public ArrayList<Blog> parseBlog(String jsonText) throws ParseException {
+        try {
+            Blogs = new ArrayList<>();
+            JSONParser j = new JSONParser();
+            Map<String, Object> ReclamationListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+
+            List<Map<String, Object>> list = (List<Map<String, Object>>) ReclamationListJson.get("root");
+
+            for (Map<String, Object> obj : list) {
+                Blog blog = new Blog();
+                float id = Float.parseFloat(obj.get("blogId").toString());
+                blog.setId((int) id);
+
+             
+
+                blog.setTitre(obj.get("titre").toString());
+                blog.setImage(obj.get("image").toString());
+                       
+          blog.setDescription(obj.get("description").toString());
+               
+                Blogs.add(blog);
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Exception in parsing reclamations ");
+        }
+
+        return Blogs;
+    }
+
+    public ArrayList<Blog> findAll() {
+        String url = Statics.BASE_URL + "/blog/MobileBlogs";
+        req.setUrl(url);
+        req.setPost(false);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                try {
+                    Blogs = parseBlog(new String(req.getResponseData()));
+                } catch (ParseException ex) {
+                 }
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return Blogs;
+    }
+}
